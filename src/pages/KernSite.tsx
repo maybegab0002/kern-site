16fd4e7d8f263000f6745275d16fca5b628a32aa
maybegab0002 <gabenciso119@gmail.com>
2026-02-23 01:09:44 +0800

    Another Mistake fix (System in hero section)

--- src/pages/KernSite.tsx
+++ src/pages/KernSite.tsx
@@ -660,7 +660,6 @@ const KernSite: React.FC = () => {
         <motion.div style={{ marginBottom: 3 }} initial={{ opacity:0, y:22 }} animate={{ opacity:1, y:0 }} transition={{ duration:0.7, delay:0.28, ease:[0.22,1,0.36,1] }}>
           <div className="htw">
             <h3 className="ht">We build software</h3>
-              <span className="htag">Systems</span>
             </div>
 
           <h3 className="ht">that actually works.</h3>
