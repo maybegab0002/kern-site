06aa10a0698608116e2b7384d51fc69bc36f7370
maybegab0002 <gabenciso119@gmail.com>
2026-03-14 14:20:58 +0800

    Removed Projects Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Kernsite from './pages/KernSite'
-import Omniportal from './projects/Omniportal'
-import Kadiz from './projects/Kadiz'
-import Spendzy from './projects/Spendzy'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Kernsite />} />
-        <Route path="/projects/Omniportal" element={<Omniportal />} />
-        <Route path="/projects/kadiz" element={<Kadiz />} />
-        <Route path="/projects/Spendzy" element={<Spendzy />} />
         <Route path="*" element={<Navigate to="/" replace />} />
         
       </Routes>
